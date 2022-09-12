a99f4d4e040d6766da366e1628559d5577fb694b
Nick Petrov <springate.nick.dev@gmail.com>
2022-09-12 09:37:33 +0300

    change eslinttrc(add rules for prettier)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -13,4 +13,24 @@ module.exports = {
       },
     },
   ],
+  rules: {
+    'react/require-default-props': 'off', // we want to use typescript to set the default properties
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/no-explicit-any': 'off', // sometimes it's necessary because react-native lacks some types
+    'linebreak-style': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off', // because __typename and _id needs to be used
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSameLine: true,
+        printWidth: 80,
+        'arrow-body-style': ['error', 'as-needed'],
+      },
+    ],
+  },
 };
