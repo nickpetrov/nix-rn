b4093711a3c224abc0f5fcb393dbadcd451189fb
Nick Petrov <springate.nick.dev@gmail.com>
2022-09-13 13:35:13 +0300

    delete unnessary getCountry api call

--- src/api/baseService/index.ts
+++ src/api/baseService/index.ts
@@ -1,17 +1,11 @@
 import apiClient from 'api';
-import axios from 'axios';
 
 // types
 import {NutritionType} from './types';
 
 const baseService = {
   async getNutrionTopics() {
-    return await apiClient.get('nutrition_topics');
-  },
-  async getCountries() {
-    return await axios.get<Array<NutritionType>>(
-      'https://d1gvlspmcma3iu.cloudfront.net/country-codes.json',
-    );
+    return await apiClient.get<Array<NutritionType>>('nutrition_topics');
   },
 };
 
