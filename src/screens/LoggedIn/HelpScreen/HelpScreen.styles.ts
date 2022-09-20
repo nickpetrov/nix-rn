d686c46b82dbbef2dd0fb72f36cf0f7ea1fbabfd
Nick Petrov <springate.nick.dev@gmail.com>
2022-09-20 16:14:56 +0300

    add HelpScreen

---
+++ src/screens/LoggedIn/HelpScreen/HelpScreen.styles.ts
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  root: {flex: 1, paddingBottom: 70, backgroundColor: '#fff'},
+  menuItem: {
+    borderBottomColor: '#bebebe',
+    borderBottomWidth: 1,
+    padding: 12,
+    backgroundColor: '#fff',
+  },
+  container: {background: '#fff', minHeight: '100%'},
+  text: {
+    margin: 10,
+    color: '#00f',
+    textDecorationStyle: 'solid',
+    textDecorationLine: 'underline',
+  },
+  terms: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  close: {
+    position: 'absolute',
+    right: 105,
+    top: 8,
+    zIndex: 10,
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    height: 40,
+    width: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#bbb',
+  },
+});
--- src/screens/LoggedIn/HelpScreen/index.tsx
+++ src/screens/LoggedIn/HelpScreen/index.tsx
@@ -1,10 +1,81 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+// utils
+import React, {useCallback, useEffect, useState} from 'react';
 
-export const HelpScreen: React.FC = () => {
+// components
+import {View, Text, TouchableWithoutFeedback, SafeAreaView} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+// styles
+import {styles} from './HelpScreen.styles';
+
+interface HelpScreenProps {}
+
+export const HelpScreen: React.FC<HelpScreenProps> = () => {
+  const [showWebView, setShowWebView] = useState(false);
+  const [webViewUri, setWebViewUri] = useState('');
+
+  const showWalkthrough = useCallback(() => {
+    console.log('walkthrough unavailable');
+  }, []);
+
+  useEffect(() => {
+    if (webViewUri.length) {
+      setShowWebView(true);
+    } else {
+      setShowWebView(false);
+    }
+  }, [webViewUri]);
+
   return (
-    <View>
-      <Text>Help Screen</Text>
-    </View>
+    <SafeAreaView style={styles.root}>
+      {!showWebView ? (
+        <View style={styles.container}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              showWalkthrough();
+            }}>
+            <View style={styles.menuItem}>
+              <Text>Walkthrough (temporary unavailable)</Text>
+              {/*TODO - create walkthrough*/}
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              setWebViewUri('https://nutritionix.helpsite.com/');
+            }}>
+            <View style={styles.menuItem}>
+              <Text>FAQ</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <View style={styles.terms}>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                setWebViewUri('https://www.nutritionix.com/terms');
+              }}>
+              <Text style={styles.text}>Terms and Conditions</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                setWebViewUri('https://www.nutritionix.com/privacy');
+              }}>
+              <Text style={styles.text}>Privacy Policy</Text>
+            </TouchableWithoutFeedback>
+          </View>
+        </View>
+      ) : (
+        <View style={{flex: 1}}>
+          <TouchableWithoutFeedback onPress={() => setWebViewUri('')}>
+            <View style={styles.close}>
+              <Text>X</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <WebView
+            // onMessage={data => handleMessageFromWebView(data)}
+            style={{width: '100%'}}
+            source={{uri: webViewUri}}
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
 };
