2d7c216ad780ee5873727b1a7f5cb40d3732a33e
Nick Petrov <springate.nick.dev@gmail.com>
2022-09-23 12:03:58 +0300

    add PreferencesMenuScreen

---
+++ src/screens/Preferences/PreferencesMenuScreen/PreferencesMenuScreen.styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  menuItem: {
+    borderBottomColor: '#bebebe',
+    borderBottomWidth: 1,
+    padding: 12,
+    backgroundColor: '#fff',
+  },
+});
--- src/screens/Preferences/PreferencesMenuScreen/index.tsx
+++ src/screens/Preferences/PreferencesMenuScreen/index.tsx
@@ -1,10 +1,79 @@
+// utils
 import React from 'react';
-import {Text, View} from 'react-native';
 
-export const PreferencesMenuScreen: React.FC = () => {
+// components
+import {View, Text, TouchableWithoutFeedback, SafeAreaView} from 'react-native';
+
+// types
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {StackNavigatorParamList} from 'navigation/navigation.types';
+
+// contants
+import {Routes} from 'navigation/Routes';
+
+// styles
+import {styles} from './PreferencesMenuScreen.styles';
+
+interface PreferencesMenuScreenProps {
+  navigation: NativeStackNavigationProp<
+    StackNavigatorParamList,
+    Routes.Preferences
+  >;
+}
+
+export const PreferencesMenuScreen: React.FC<PreferencesMenuScreenProps> = ({
+  navigation,
+}) => {
   return (
-    <View>
-      <Text>Preferences Menu Screen</Text>
-    </View>
+    <SafeAreaView>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate(Routes.Profile);
+        }}>
+        <View style={styles.menuItem}>
+          <Text>My Profile</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate(Routes.DailyCalories);
+        }}>
+        <View style={styles.menuItem}>
+          <Text>Daily Calorie Preferences</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate(Routes.ConnectedApps);
+        }}>
+        <View style={styles.menuItem}>
+          <Text>Connected Apps</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate(Routes.Notifications);
+        }}>
+        <View style={styles.menuItem}>
+          <Text>Notifications</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate(Routes.GroceryAgentSettings);
+        }}>
+        <View style={styles.menuItem}>
+          <Text>Grocery Agent Preferences</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate(Routes.DeveloperSettings);
+        }}>
+        <View style={styles.menuItem}>
+          <Text>Developer settings</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 };
