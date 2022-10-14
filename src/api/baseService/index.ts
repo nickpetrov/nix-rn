42022c039decb1c9ecab6d158fc7d9e6ffbe12e5
Nick Petrov <springate.nick.dev@gmail.com>
2022-10-14 11:25:06 +0300

    add chages to api call that search barcodes

--- src/api/baseService/index.ts
+++ src/api/baseService/index.ts
@@ -15,18 +15,16 @@ const baseService = {
     );
   },
   async getFoodByQRcode(searchValue: string) {
-    return await apiClient.get<{foods: Array<FoodProps>}>(
-      `/search/item?upc=${searchValue}`,
-      {
-        headers: {
-          'x-3scale-bypass': 'c49e69471a7b51beb2bb0e452ef53867385f7a5a',
-        },
-        data: {
-          appKey: '59bcfe12c0e9965162798a31ff38ec1f',
-          appId: '906641bd',
-        },
+    return await apiClient.get<{foods: Array<FoodProps>}>(`/search/item`, {
+      params: {
+        upc: searchValue,
       },
-    );
+      headers: {
+        'x-3scale-bypass': 'c49e69471a7b51beb2bb0e452ef53867385f7a5a',
+        'x-app-key': '59bcfe12c0e9965162798a31ff38ec1f',
+        'x-app-id': '906641bd',
+      },
+    });
   },
   async getSuggestedFood() {
     return await apiClient.get('recommended_products.json.gz', {
